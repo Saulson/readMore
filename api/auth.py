f02890765d4f7b044b0129fa396a999afc912a1a
Saulson <saulson.96@gmail.com>
2019-05-13 12:59:32 -0500

    Se modifica el parametro que se manda al query

--- api/auth.py
+++ api/auth.py
@@ -13,20 +13,18 @@ def login():
     data = {}
     if request.method == 'POST':
         cur = get_cursor()
-        username = request.form['user']
-        password = request.form['password']
 
         try:
             cur.execute("""
-                SELECT id, contrasena FROM usuario WHERE id = %s
-            """, (username,))
+                SELECT id, contrasena FROM usuario WHERE id = %(user)s
+            """, request.form)
         except psycopg2.Error as ex:
             data.update(error=ex.pgerror, status=400)
         else:
             user = cur.fetchone()
             if user is None:
                 data.update(error="Incorrect Username", status=400)
-            elif not check_password_hash(user['contrasena'], password):
+            elif not check_password_hash(user['contrasena'], request.form['password']):
                 data.update(error="Incorrect Password", status=400)
             else:
                 session.clear()
@@ -45,11 +43,24 @@ def logout():
     response.headers.set('Content-Type', 'application/json; charset=UTF-8')
     return response
 
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        cur = get_cursor()
+        cur.execute("SELECT * FROM usuario WHERE id = %s", (user_id,))
+        g.user = cur.fetchone()
+
 def session_check(funct):
     @functools.wraps(funct)
     def wrapper(**kwargs):
         if g.user is None:
-            return "Error"
+            response = make_response(None, 401)
+            response.headers.set('Content-Type', 'application/json; charset=UTF-8')
+            return response
 
         return funct(**kwargs)
 
