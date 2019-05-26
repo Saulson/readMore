94ff8b85e084e409a42b53d08ac89379562ed9bb
Saulson <saulson.96@gmail.com>
2019-05-26 13:26:29 -0500

    Se mejora el api creando funciones genericas para las operaciones CRUD

--- api/editorial.py
+++ api/editorial.py
@@ -1,32 +1,17 @@
 import psycopg2
 
-from flask import Blueprint, current_app, jsonify, make_response, request
+from flask import Blueprint, jsonify, make_response, request
 
-from db import get_db, get_cursor
+from db import get_db
 from auth import session_check
+import process_request
 
 bp = Blueprint('editorial', __name__, url_prefix='/editorial')
 
 @bp.route('/num', methods=('GET',))
 @session_check
 def count():
-    data = {}
-    cur = get_cursor()
-    query = "SELECT COUNT(*) FROM editorial"
-
-    try:
-        cur.execute(query)
-    except pgerror as ex:
-        data.update(error=ex.error, status=400)
-    else:
-        data.update(data=cur.fetchone(), status=200)
-
-    response = make_response(jsonify(data), data.get('status', 400))
-    response.headers.set('Content-Type', 'application/json; charset=UTF-8')
-
-    return response
-
-
+    return process_request.count('editorial')
 
 @bp.route('/', methods=('GET', 'PUT', 'PATCH', 'DELETE'))
 @bp.route('', methods=('GET', 'PUT', 'PATCH', 'DELETE'))
@@ -36,16 +21,16 @@ def editorial():
     data = {}
 
     if method == 'GET':
-        data = get()
+        data = process_request.get('editorial')
 
     elif method == 'PUT':
-        data = put()
+        data = process_request.put('editorial', ['nombre'])
 
     elif method == 'PATCH':
-        data = patch()
+        data = process_request.patch('editorial', ['nombre'])
 
     elif method == 'DELETE':
-        data = delete()
+        data = process_request.delete('editorial')
 
     if data.get('status', 400) == 200:
         get_db().commit()
@@ -54,79 +39,3 @@ def editorial():
     response.headers.set('Content-Type', 'application/json; charset=UTF-8')
 
     return response
-
-def get():
-    data = {}
-    cur = get_cursor()
-    args = dict(request.args)
-    query = "SELECT * FROM editorial "
-    if args:
-        keys = list(args)
-        if 'limit' in keys:
-            keys.remove('limit')
-
-        if 'offset' in keys:
-            keys.remove('offset')
-
-        if keys:
-            query += "WHERE "
-            for key in keys:
-                    query += key + " = %(" + key + ")s AND "
-
-            query = query[:-4]
-
-        if 'limit' in args:
-            args['limit'] = int(args['limit'])
-            query += "LIMIT %(limit)s "
-
-        if  'offset' in args:
-            args['offset'] = int(args['offset'])
-            query += "OFFSET %(offset)s "
-
-    try:
-        cur.execute(query, args)
-    except psycopg2.Error as ex:
-        data.update(error=ex.pgerror, status=400)
-    else:
-        data.update(data=cur.fetchall(), status=200)
-
-    return data
-
-def put():
-    data = {}
-    cur = get_cursor()
-
-    try:
-        cur.execute("INSERT INTO editorial (nombre) VALUES (%(nombre)s)", request.form)
-    except psycopg2.Error as ex:
-        data.update(error=ex.pgerror, status=400)
-    else:
-        data.update(data={'message': cur.statusmessage}, status=200)
-
-    return data
-
-def patch():
-    data = {}
-    cur = get_cursor()
-
-    try:
-        cur.execute("UPDATE editorial SET nombre = %(nombre)s WHERE id = %(id)s", request.form)
-    except psycopg2.Error as ex:
-        data.update(error=ex.pgerror, status=400)
-    else:
-        data.update(data={'message': cur.statusmessage}, status=200)
-
-    return data
-
-def delete():
-    data = {}
-    cur = get_cursor()
-
-    try:
-        cur.execute("DELETE FROM editorial WHERE id = %(id)s", request.args)
-    except psycopg2.Error as ex:
-        data.update(error=ex.pgerror, status=400)
-    else:
-        data.update(data={'message': cur.statusmessage}, status=200)
-
-    return data
---
+++ api/process_request.py
@@ -0,0 +1,149 @@
+import psycopg2
+
+from flask import jsonify, make_response, request
+
+from db import get_cursor
+
+def count(table_name):
+    data = {}
+    cur = get_cursor()
+    query = "SELECT COUNT(*) FROM %s" % table_name
+
+    try:
+        cur.execute(query)
+    except pgerror as ex:
+        data.update(error=ex.error, status=400)
+    else:
+        data.update(data=cur.fetchone(), status=200)
+
+    response = make_response(jsonify(data), data.get('status', 400))
+    response.headers.set('Content-Type', 'application/json; charset=UTF-8')
+
+    return response
+
+def get(table_name):
+    data = {}
+
+    if not table_name:
+        data.update(
+            error="Bad Request", 
+            status=400
+        )
+        return data
+
+    cur = get_cursor()
+    args = dict(request.args)
+    query = "SELECT * FROM %s " % table_name
+    if args:
+        keys = list(args)
+        if 'limit' in keys:
+            keys.remove('limit')
+
+        if 'offset' in keys:
+            keys.remove('offset')
+
+        if keys:
+            query += "WHERE "
+            for key in keys:
+                    query += key + " = %(" + key + ")s AND "
+
+            query = query[:-4]
+
+        if 'limit' in args:
+            args['limit'] = int(args['limit'])
+            query += "LIMIT %(limit)s "
+
+        if  'offset' in args:
+            args['offset'] = int(args['offset'])
+            query += "OFFSET %(offset)s "
+
+    try:
+        cur.execute(query, args)
+    except psycopg2.Error as ex:
+        data.update(error=ex.pgerror, status=400)
+    else:
+        data.update(data=cur.fetchall(), status=200)
+
+    return data
+
+def put(table_name, fields):
+    data = {}
+
+    if not fields or not table_name:
+        data.update(
+            error="Bad Request", 
+            status=400
+        )
+        return data
+
+    cur = get_cursor()
+    query = "INSERT INTO %s (" % table_name
+
+    for field in fields:
+        query += field + ", "
+
+    query = query[:-2] + ") VALUES ("
+
+    for field in fields:
+        query += "%(" + field + ")s, "
+
+    query = query[:-2] + ")"
+
+    try:
+        cur.execute(query, request.form)
+    except psycopg2.Error as ex:
+        data.update(error=ex.pgerror, status=400)
+    else:
+        data.update(data={'message': cur.statusmessage}, status=200)
+
+    return data
+
+def patch(table_name, fields):
+    data = {}
+
+    if not fields or not table_name:
+        data.update(
+            error="Bad Request", 
+            status=400
+        )
+        return data
+
+    cur = get_cursor()
+    query = "UPDATE %s SET " % table_name
+
+    for field in fields:
+        query += field + " = %(" + field + ")s "
+
+    query += "WHERE id = %(id)s"
+
+    try:
+        cur.execute(query, request.form)
+    except psycopg2.Error as ex:
+        data.update(error=ex.pgerror, status=400)
+    else:
+        data.update(data={'message': cur.statusmessage}, status=200)
+
+    return data
+
+def delete(table_name):
+    data = {}
+
+    if not table_name:
+        data.update(
+            error="Bad Request", 
+            status=400
+        )
+        return data
+
+    cur = get_cursor()
+    query = "DELETE FROM %s WHERE " % table_name
+    query += "id = %(id)s"
+
+    try:
+        cur.execute(query, request.args)
+    except psycopg2.Error as ex:
+        data.update(error=ex.pgerror, status=400)
+    else:
+        data.update(data={'message': cur.statusmessage}, status=200)
+
+    return data
